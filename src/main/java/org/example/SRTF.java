bca8f06907d5ee61690d437f7b24624dc4af234c
Maria-alfonse <125464100+Maria-alfonse@users.noreply.github.com>
2024-12-05 16:51:59 +0200

    Add files via upload

---
+++ src/main/java/org/example/SRTF.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.List;
+
+public class SRTF implements Scheduler{
+    @Override
+    public void execute(List<Process> processes,  int context) {
+
+    }
+}
